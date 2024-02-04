13813634c6a675484cd0c34f0157d49161165121
hp <tghamzaff@gmail.com>
2024-02-05 03:15:39 +0500

    Added a select box

--- pages/Contact Us.py
+++ pages/Contact Us.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.header("Contact Us")
+
+with st.form(key="form_key"):
+    user_email = st.text_input("Enter your email")
+    choice = st.selectbox("What topic do you want to discuss?", ("Job Inquiries", "Project Proposals", "Other"))
+    user_message = st.text_area("Enter your message")
+    button = st.form_submit_button("Submit")
